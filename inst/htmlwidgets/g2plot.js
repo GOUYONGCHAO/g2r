d24fce0b6993a9b0d65863d7ba397e89b712b429
YONGCHAO GOU <gouyongchao@YONGCHAOdeMacBook-Pro.local>
2020-03-01 16:08:11 +0800

    修改文件夹

---
+++ inst/htmlwidgets/g2plot.js
@@ -0,0 +1,36 @@
+HTMLWidgets.widget({
+
+    name: 'g2plot',
+  
+    type: 'output',
+  
+    factory: function (el, width, height) {
+  
+      const chart = new G2.Chart({
+        container: document.getElementById("g2plot"), // 指定图表容器 ID
+        width: width, // 指定图表宽度
+        height: height, // 指定图表高度
+        data: [
+          { genre: 'Sports', sold: 275 },
+          { genre: 'Strategy', sold: 115 },
+          { genre: 'Action', sold: 120 },
+          { genre: 'Shooter', sold: 350 },
+          { genre: 'Other', sold: 150 },
+        ]
+      });
+      return {
+  
+        renderValue: function (x) {
+          chart.interval().position('genre*sold')
+          chart.render();
+        },
+  
+        resize: function (width, height) {
+  
+          // TODO: code to re-render the widget with a new size
+  
+        }
+      };
+    },
+  }
+  );
