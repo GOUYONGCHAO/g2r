f51eb5ddd676b29fdb9f3cc2fefb7e5ffc2e02a8
YONGCHAO GOU <gouyongchao@YONGCHAOdeMacBook-Pro.local>
2020-03-01 16:23:50 +0800

    test success

--- inst/htmlwidgets/g2plot.js
+++ inst/htmlwidgets/g2plot.js
@@ -9,21 +9,21 @@ HTMLWidgets.widget({
       const chart = new G2.Chart({
         container: document.getElementById("g2plot"), // 指定图表容器 ID
         width: width, // 指定图表宽度
-        height: height, // 指定图表高度
-        data: [
-          { genre: 'Sports', sold: 275 },
-          { genre: 'Strategy', sold: 115 },
-          { genre: 'Action', sold: 120 },
-          { genre: 'Shooter', sold: 350 },
-          { genre: 'Other', sold: 150 },
-        ]
+        height: height // 指定图表高度
       });
+      const data = [
+        { genre: 'Sports', sold: 275 },
+        { genre: 'Strategy', sold: 115 },
+        { genre: 'Action', sold: 120 },
+        { genre: 'Shooter', sold: 350 },
+        { genre: 'Other', sold: 150 },
+      ];
       return {
-  
         renderValue: function (x) {
           chart.data(data);
           chart.interval().position('genre*sold');
           chart.render();
+          el=chart;
         },
   
         resize: function (width, height) {
